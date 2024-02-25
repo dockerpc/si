e51418ea410eb22d141e82e838f427f7c009ee06
sithu@sithu.com <sithu@sithu.com>
2024-02-25 06:39:56 -0700

    changes added to test version ids

---
+++ listintroduction.py
@@ -0,0 +1,13 @@
+students = ["Mg Mg","90","Aung","70","Kyaw","60","Aye"]
+# student_name[0] = "Min"
+#print (student_name)
+number_of_list = len(students)
+print(number_of_list)
+#first_student = students[0]
+#print(first_student)
+#print (f'Before appending : {students}')
+#students.append("Min")
+#print(f'After appending : {students}')
+last_student = students.pop()
+print(students)
+print(last_student)
