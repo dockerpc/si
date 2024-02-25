610b74280186a71ce1f82c470397253983ba35d2
sithu@sithu.com <sithu@sithu.com>
2024-02-25 06:30:14 -0700

    first commit

---
+++ list.py
@@ -0,0 +1,7 @@
+students = ['Aung','Maung','Kyaw','Paing']
+#print (f"Before appending : {students}")
+#students.append('Hla')
+#print (f"After appending : {students}")
+last_student = students.pop()
+print(students)
+print(last_student)
